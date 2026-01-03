2bd8352343b7580498b9c8bca87c1561948c0e3f
Ashutosh Modi <modiashutoshd@gmail.com>
2026-01-02 21:07:34 -0500

    Create __init__.py

---
+++ spark_jobs/__init__.py
@@ -0,0 +1,8 @@
+"""
+Project Initialization Module
+==============================
+This file makes the spark_jobs directory a Python package.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Spark-Airflow Learning Project"
